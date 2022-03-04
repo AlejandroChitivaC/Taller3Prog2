86bf1ded8130e74dc0a5c6178d98b7cc61d8f964
AlejandroChitivaC <achitivac@unbosque.edu.co>
2022-03-03 20:35:26 -0500

    added a jsonfile and social.html was modified

---
+++ scripts/index.js
@@ -0,0 +1,37 @@
+const url = " http://localhost:1000/obrasarte/"
+let campo = document.querySelector("#socialcard")
+// se trae la info del .json
+const getData = async () => {
+
+    const resp = await fetch(url);
+    const data = await resp.json();
+    data.forEach(card => {
+        const { nombre, imagen, precio } = card;
+        campo.innerHTML += ` 
+        <div class="card">
+                <img src="${imagen}" alt="">
+                <p>${nombre}</p>
+                <p>${precio}</p>
+            </div>
+        `
+    })
+}
+
+window.addEventListener('DOMContentLoaded', getData);
+
+
+// se crea una funcion para mostrar la info traída del json al html
+//const showInfo (data,campo ) donde data es lo que se a mandar al html y el campo es en que etiqueta se va mostrar
+// const showInfo = async (data, campo) => {
+//     const cards = await data;
+//     cards.forEach(card => {
+//         const { nombre, imagen } = card;
+//         campo.innerHTML += ` 
+//         <div class="card">
+//                 <img src="${imagen}" alt="">
+//                 <p>${nombre}</p>
+//             </div>
+//         `
+//     })
+
+// }
