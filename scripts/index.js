e7d3eb1366531a0ea00b4a2f4674c33c85a54c59
Alejandro Chitiva Castillo <83592808+AlejandroChitivaC@users.noreply.github.com>
2022-03-07 20:04:42 -0500

    contador

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,14 @@
 const url = " https://db-alejandro.herokuapp.com/obrasarte/"
 let campo = document.querySelector("#socialcard")
+//contador likes
+let contador=0;
+let msg="Solo puedes dar un clic por obra de arte"
+function contador(){
+    document.getElementById("btnlikes").innerHTML=contador++;
+    if(counter>1){
+        alert(msg)
+    }
+}
 // se trae la info del .json
 const getData = async () => {
 
@@ -15,7 +24,7 @@ const getData = async () => {
                 <p style="color:blue; font-weight:bold">${"@" + nombre}</p>
                 <p>${precio}</p>
                 <p>${"Categoría: " + categoria}</p>
-                <button id="btnlikes" class="btn btn-primary" style="border-radius:50px">${likes} Like</button>
+                <button id="btnlikes" class="btn btn-primary" style="border-radius:50px" >${likes}Like</button>
             </div> 
         `
 
